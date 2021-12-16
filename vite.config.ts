233a2e3cddbe38066601ce256937e2cad8597ca5
Heron Amaral <heron.amaral@gmail.com>
2021-12-16 19:25:12 -0300

    Adding deploy.sh

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://heronoa.github.io/frontend-challenge1-pessoatech-/"
+  base:"/<REPO>/"
 })
